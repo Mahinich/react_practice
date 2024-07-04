e1e425295b9209f6fe7036f60f3f2bb6807f624b
Mahinich <andrey.mahinitch@gmail.com>
2024-07-04 08:53:40 +0300

    unit_08-hw/done

--- unit_08-hw/src/App.js
+++ unit_08-hw/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
-const h1 = React.createElement('h1', {}, 'unit_08.');
-const h2 = React.createElement('h1', {style:{color: 'orange'}}, 'header 2');
-const p = React.createElement('p', {style: {color: 'red'}}, 'this is p');
-const input = React.createElement('input', {defaultValue: '55'});
-const p1 = React.createElement('p',{}, 'hi');
-const p2 = React.createElement('p',{}, 'world');
-const div = React.createElement('div', {className: 'text-grey'}, p1, p2)
 
 function App() {
+
+  const h1 = React.createElement('h1', {}, 'unit_08.');
+  const h2 = React.createElement('h2', {style:{color: 'orange'}}, 'header 2');
+  const p = React.createElement('p', {style: {color: 'red'}}, 'this is p');
+  const input = React.createElement('input', {defaultValue: '55'});
+  const p1 = React.createElement('p',{}, 'hi');
+  const p2 = React.createElement('p',{}, 'world');
+  const div = React.createElement('div', {className: 'text-grey'}, p1, p2)
+  
+  let [list, setList] = useState([]);
+  const inpRef = React.createRef();
+  const inp = React.createElement('input', {ref: inpRef});
+  const btn = React.createElement('button', {
+    onClick: () => {
+      setList([...list, inpRef.current.value]);
+      inpRef.current.value = '';
+    }
+  }, 'push')
+  
   return (
    <>
     <React.Fragment>
@@ -18,6 +30,12 @@ function App() {
       {p}
       {input}
       {div}
+      {inp}
+      {btn}
+
+      <ul>
+        {list.map(item => <li key={item}>{`${item} `}</li>)}
+      </ul>
     </React.Fragment>
    </>
   );
